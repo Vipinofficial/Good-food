ed95f34b854aa0db1328dbacd1caf87160dec53f
Vipin Kumar Soni <70593446+Vipinofficial@users.noreply.github.com>
2025-10-24 15:57:42 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Good-food',
+  description: 'Copyright Good-food ',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
