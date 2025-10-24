2e3abbcedc7d8a12df73a881e7702f2d6b848b79
Vipin Kumar Soni <70593446+Vipinofficial@users.noreply.github.com>
2025-10-24 16:12:50 +0530

    Revert "."
    This reverts commit ed95f34b854aa0db1328dbacd1caf87160dec53f.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Good-food',
-  description: 'Copyright Good-food ',
-  generator: 'Next.js',
+  title: 'v0 App',
+  description: 'Created with v0',
+  generator: 'v0.app',
 }
 
 export default function RootLayout({
